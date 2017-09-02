34338a1ce38df9b7c6d00e12cce66ea837631a8f
caramirezal <cramireza@ciencias.unam.mx>
2017-09-02 17:12:28 -0500

    filtro de fastq files

--- pipeLine.R
+++ pipeLine.R
@@ -3,25 +3,35 @@
 ## dependencias
 library(Rqc)
 library(knitr)
+library(ShortRead)
 
+
+#############################################################################
 ## control de calidad
-dir <- "data/"
-files <- list.files("data/")
+dir <- "data/fastq/"
+files <- list.files(dir)
 path <- paste(dir,files,sep = "")
+#rqc <- rqc(path = dir,pattern = ".gz")
 qa <- rqcQA(path,workers=1)
 
 ## porcentaje de lecturas con calidad mayor
 ## que el umbral de calidad
+png("figures/qualityAboveThreshold.png")
 rqcReadQualityPlot(qa)
+dev.off()
 
 ## porcentaje de GC por ciclos de amplificación
+png("figures/gcPerCycle.png")
 rqcCycleGCPlot(qa)
+dev.off()
 
 ## porcentaje de quality calls por ciclo
 ## amplificación
+png("figures/qcPerCycle.png")
 rqcCycleQualityPlot(qa)
+dev.off()
 
 
-
-
+#############################################################################
+## Filtrado de datos
 
